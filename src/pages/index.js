e7c346e6703de15275617e96caa63e78306c0fb6
leandroperego <leperego@hotmail.com>
2024-10-28 21:00:44 -0300

    Usando o Metadata do gatsby-config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,21 @@
 import * as React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 
 export default function Index() {
+
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }`);
+
   return (
     <main>
       <h1>Home Page</h1>
+      <h2>Segundo titulo com o title do gatsby-config: {data.site.siteMetadata.title}</h2>
       <p>Aqui é a home page!</p>
       <p>Segundo paragrafo criado para dar novo push e observar no netlify o comportamento</p>
     </main>
